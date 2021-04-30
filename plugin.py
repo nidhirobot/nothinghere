73a9cefc680b4898ae583e6080c528ba85db3ada
Aju <73631177+nidhirobot@users.noreply.github.com>
2021-04-30 09:07:54 +0530

    Create plugin.py

---
+++ plugin.py
@@ -0,0 +1,26 @@
+from telethon import events
+from telethon.errors.rpcerrorlist import YouBlockedUserError
+
+from . import *
+
+@ultroid_cmd(pattern="https$")
+async def _(event):
+	if event.fwd_from:
+		return
+    chat = "@hqproxy_bot"
+    msg = await eor(ult, "Sending Your Https Proxies...")
+    async with ultroid_bot.conversation(chat) as conv:
+        try:
+            response = conv.wait_event(
+                events.NewMessage(incoming=True, from_users=1683047947)
+            )
+            await conv.send_message("/https")
+            response = await response
+        except YouBlockedUserError:
+            await msg.edit("Boss! Please Unblock @hqproxy_bot ")
+            return
+        if response.text.startswith(" "):
+            await event.edit("That bot is dead bro now this cmd is useless ðŸ˜‚ðŸ˜‚")
+        else: 
+            await event.delete()
+            await event.client.send_message(event.chat_id, response.message)
